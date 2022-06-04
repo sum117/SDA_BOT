26940096466c1a0d3243ac097787f063d8a73259
mangowhite <jvcaliman@hotmail.com>
2022-06-04 04:30:18 -0300

    Criando branch para refatoração

---
+++ src/bot/events/messageCreate/messageCreate.js
@@ -0,0 +1,12 @@
+const {Message} = require('discord.js');
+module.exports = {
+    name: 'messageCreate',
+    /**
+     * Evento de criação de mensagens.
+     * @param {Message} msg A mensagem que foi postada.
+     * @returns {Object} O objeto da mensagem.
+     */
+    async execute(msg) {
+        
+    }
+}
