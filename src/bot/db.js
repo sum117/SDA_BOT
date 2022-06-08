00bb3b1d4723afffef02373a0af33aebd6f841a2
sum117 <jvcaliman@hotmail.com>
2022-06-08 23:38:50 +0200

    test

--- src/bot/db.js
+++ src/bot/db.js
@@ -1,6 +1,7 @@
 
 const Sequelize = require('sequelize')
-const database = new Sequelize('sda', 'postgres', 'admin', {
+const config = require('../../config.json')
+const database = new Sequelize(config.database.name, config.database.user, config.database.password, {
     host: 'localhost',
     dialect: 'postgres',
 })
